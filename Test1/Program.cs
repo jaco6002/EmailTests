0113d14487dcf248e377c51df912cc464073d765
jaco6002 <jaco6002@edu.easj.dk>
2020-12-08 23:56:44 +0100

    EmailWithAttachment

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
 
 namespace Test1
 {
@@ -14,7 +15,8 @@ namespace Test1
             recievers.Add("");
             //recievers.Add("");
             //recievers.Add("");
-            Email(message, recievers);
+            //Email(message, recievers);
+            EmailWithAttachment(message, recievers);
         }
         public static void Email(string messageString, List<string> recievers)
         {
@@ -30,6 +32,43 @@ namespace Test1
                 {
                     message.To.Add(new MailAddress(reciever));
                 }
+                message.Subject = "Test";
+                //message.IsBodyHtml = true; //to make message body as html  
+                message.Body = messageString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(FromMail, FromMailPW);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+                Console.WriteLine("email(s) sent");
+            }
+            catch (Exception) { }
+        }
+        public static void EmailWithAttachment(string messageString, List<string> recievers)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                string FromMail = "";
+                string FromMailPW = "";
+                message.From = new MailAddress(FromMail);
+                //message.To.Add(new MailAddress(""));
+                foreach (var reciever in recievers)
+                {
+                    message.To.Add(new MailAddress(reciever));
+                }
+
+                #region attachment
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //url til desktop
+                string file = System.IO.Path.Combine(desktopPath, "CV til praktik.docx");//url+filnavn fra desktop
+                Attachment data = new Attachment(file, MediaTypeNames.Application.Octet);
+                // Add the file attachment to this email message.
+                message.Attachments.Add(data);
+                #endregion
+
                 message.Subject = "Test";
                 //message.IsBodyHtml = true; //to make message body as html  
                 message.Body = messageString;
